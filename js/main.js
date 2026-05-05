0aa2d76f2c008e6669ecda6862d39f7ae8543253
jiaotou11 <3193419029@qq.com>
2026-05-06 04:09:06 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 function closePopup(){
-    document.getElementById("popup").style.display = "none";
+    document.getElementById("popup").style.display="none";
+}
+
+function copyText(text){
+    navigator.clipboard.writeText(text);
+    alert("复制成功");
 }
